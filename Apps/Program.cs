e9f5a8bdcf88c55ad71b834490616059a02f3d10
Jim Smits <james.smits@gmail.com>
2021-02-12 20:32:13 -0500

    Day 11 belated.  Having a great time.

--- Apps/Program.cs
+++ Apps/Program.cs
@@ -5,6 +5,7 @@ namespace App
 {
     class Program
     {
+        static decimal _leftOperand;
         static void Main(string[] args)
         {
             //round one - we are going to support simple arthimetic, +,-,*/.
@@ -16,7 +17,6 @@ namespace App
 
             while (true)
             {
-                //Console.ResetColor();
                 var input = Console.ReadLine().ToLower();
                 if (input == "exit")
                 {
@@ -25,21 +25,58 @@ namespace App
 
                 try
                 {
-                    ExpressionEvaluator evaluator = new ExpressionEvaluator(input);
-                    Console.WriteLine(new Calc(evaluator).PerformCalulation());
+                    //TODO: keyword fun.
+                    if (input == "pi" || input == "π")
+                    {
+                        Console.WriteLine(Math.PI);
+                    }
+                    else
+                    {
+                        ExpressionEvaluator evaluator = new ExpressionEvaluator(input);
+                        _leftOperand = evaluator.Left;
+                        Console.WriteLine(new Calc(evaluator).PerformCalulation());
+                    }
                 }
                 catch (DivideByZeroException)
                 {
-                    Console.WriteLine("Error: Cannot divide by zero per laws of the universe.");
-                    Console.BackgroundColor = have fun with this
-                    Console.ForegroundColor = have fun with this
-                    
+                    //have fun with this
+                    //have fun with this
+                    int strobeFor = new Random().Next(7, 17);
+                    for (int i = 0; i <= strobeFor; i++)
+                    {
+                        Joggle();
+                        Console.SetCursorPosition(0, Console.CursorTop);
+                        DivvyNothing(strobeFor == i);
+                        System.Threading.Thread.Sleep(new Random().Next(7, 777));
+                    }
                 }
                 catch (Exception)
                 {
+                    //TODO: Fun with Invalid Expressions.
                     throw;
                 }
+            }
 
+            static void DivvyNothing(bool writeLine)
+            {
+                if (!writeLine)
+                {
+                    Console.Write($"Error: Cannot divide {_leftOperand} by zero per laws of the universe.");
+                    return;
+                }
+
+                Console.WriteLine($"Error: Cannot divide {_leftOperand} by zero per laws of the universe.");
+            }
+        }
+
+        private static void Joggle()
+        {
+            Console.ForegroundColor = (ConsoleColor)new Random().Next(0, 15);
+            Console.BackgroundColor = (ConsoleColor)new Random().Next(0, 15);
+            
+            while (Console.ForegroundColor == Console.BackgroundColor)
+            {
+                Console.ForegroundColor = (ConsoleColor)new Random().Next(0, 15);
             }
         }
     }
